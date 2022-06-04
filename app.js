1e0e38959afa6b156455dd55314a77c522fa2522
Nathanael-R <nathanielwealth20@gmail.com>
2022-06-04 16:45:43 +0100

    .

---
+++ app.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+    const navBox = document.getElementsByClassName('navbox')[0]
+    const navUl = document.getElementsByClassName('links')[0]
+
+    navBox.addEventListener('click', () => {
+        navUl.classList.toggle('active')
+    
+    })
+}
+
+navSlide()
+
